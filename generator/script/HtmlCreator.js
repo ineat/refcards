cf4e941f2af5b95f9a0f8d03414edff9530be5cf
lulutche <80798744+lulutche@users.noreply.github.com>
2021-04-30 12:00:28 +0200

    feat(generator): add metadata extractor to manage refcard colors

--- generator/script/HtmlCreator.js
+++ generator/script/HtmlCreator.js
@@ -10,6 +10,60 @@ function getTitle(path) {
 }
 
 function htmlGenerator(path){
+    let title = getTitle(path);
+    let text = fs.readFileSync(path, 'utf8');
+    const HtmlBase = `<!DOCTYPE html>\n \n<html>\n \n   <head>\n     <title> ${title} Refcard</title>\n     <link rel='stylesheet' href='css/style.css'>\n   </head>\n<body>\n`;
+    return HtmlBase +marked(text, {renderer:render_upgrade(metadataExtractor(path))}) + "\n </body>\n </html>";
+}
+
+function createFolder() {
+    fs.mkdir('public/git', (err) => {
+        logger.info("Refcard Git créée!");
+    });
+
+    fs.mkdir('public/git/assets', (err) => {
+        logger.info("Refcard Git créée!");
+    });
+    try {
+        fse.copySync('../git/assets', 'public/git/assets', {overwrite: true});
+    } catch (err) {
+        if (err) throw err;
+    }
+
+}
+
+function metadataExtractor(path) {
+    let text = fs.readFileSync(path,'utf-8');
+    const match = text.match(/(\[\/\/\]: #) \(color1:(#\w*);color2:(#\w*)\)/);
+    return [match[2],match[3]];
+}
+
+function refcardCreator(path) {
+    logger.info('Starting refcard generation');
+    const pathStruct = {
+        title : getTitle(path)
+    }
+    let text = htmlGenerator(path)
+    fs.writeFile(`public/${pathStruct.title}.html`,text,function(err){
+        if (err) throw err;
+        logger.info(`Refcard ${pathStruct.title} generated!`);
+    })
+}
+
+
+module.exports = {createFolder,refcardCreator,metadataExtractor,htmlGenerator,getTitle};
+const marked = require('marked');
+const fs =require ('fs');
+const fse = require('fs-extra');
+const {render_upgrade} = require('./CustomRenderer');
+const logger = require('./Logger');
+
+function getTitle(path) {
+    const match = path.match(/(\.\.\/)(\w*)\//);
+    return match[2];
+}
+
+function htmlGenerator(path){
     return `<!DOCTYPE html>
 <html lang="${getTitle(path)[1]}">
     <head>
